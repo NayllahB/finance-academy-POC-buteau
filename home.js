78f24a88d761ce832c454e28602cefd3ac9787ce
Nayllah Buteau <nayllah_buteau@outlook.com>
2026-05-17 22:39:38 -0400

    updating final code

---
+++ home.js
@@ -0,0 +1,9 @@
+
+//This function is called when user clicks on buttons placed on homepage
+//Both buttons take user to lesson.html but information displayed will differ depending on lessonId
+function navigateToLesson (lessonId){
+    window.location.href = `lesson.html?lesson=${lessonId}`
+}
+
+
+
